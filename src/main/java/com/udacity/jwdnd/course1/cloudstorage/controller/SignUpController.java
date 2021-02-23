3af13c6b835d7e9a486f38c16d2be5bd877feee5
Cédric Morier-Roy <cmorierroy@gmail.com>
2021-02-23 15:36:50 -0600

    disables whitelabel error and fixes max file size, adds redirect to login upon successful signup

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -26,7 +27,7 @@ public class SignUpController
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
@@ -41,7 +42,9 @@ public class SignUpController
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            //model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("signupSuccess","success");
+            return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
         }
